8721634ff68629080ce6547fb8dfba441ba6032c
yosri <halaby3737@gmail.com>
2025-06-23 19:25:01 +0300

    fairest

--- lib/config/app_text_style.dart
+++ lib/config/app_text_style.dart
@@ -7,37 +7,50 @@ class AppTextStyle {
   AppTextStyle(this.context);
 
   TextStyle get title => TextStyle(
-        color: colors(context).headingColor,
-        fontSize: 24.sp,
-        fontWeight: FontWeight.w500,
-      );
+    fontFamily: 'Cairo',
+    color: colors(context).headingColor,
+    fontSize: 24.sp,
+    fontWeight: FontWeight.w500,
+  );
+
   TextStyle get subTitle => TextStyle(
-        color: colors(context).headingColor,
-        fontSize: 18.sp,
-        fontWeight: FontWeight.w700,
-      );
+    fontFamily: 'Cairo',
+    color: colors(context).headingColor,
+    fontSize: 18.sp,
+    fontWeight: FontWeight.w700,
+  );
+
   TextStyle get bodyText => TextStyle(
-        color: colors(context).bodyTextColor,
-        fontSize: 14.sp,
-        fontWeight: FontWeight.w400,
-      );
+    fontFamily: 'Cairo',
+    color: colors(context).bodyTextColor,
+    fontSize: 14.sp,
+    fontWeight: FontWeight.w400,
+  );
+
   TextStyle get bodyTextSmall => TextStyle(
-        color: colors(context).bodyTextSmallColor,
-        fontSize: 12.sp,
-        fontWeight: FontWeight.w500,
-      );
+    fontFamily: 'Cairo',
+    color: colors(context).bodyTextSmallColor,
+    fontSize: 12.sp,
+    fontWeight: FontWeight.w500,
+  );
+
   TextStyle get buttonText => TextStyle(
-        fontSize: 16.sp,
-        fontWeight: FontWeight.w700,
-      );
+    fontFamily: 'Cairo',
+    fontSize: 16.sp,
+    fontWeight: FontWeight.w700,
+  );
+
   TextStyle get hintText => TextStyle(
-        color: colors(context).hintTextColor,
-        fontSize: 21.sp,
-        fontWeight: FontWeight.w300,
-      );
+    fontFamily: 'Cairo',
+    color: colors(context).hintTextColor,
+    fontSize: 21.sp,
+    fontWeight: FontWeight.w300,
+  );
+
   TextStyle get appBarText => TextStyle(
-        color: colors(context).headingColor,
-        fontSize: 18.sp,
-        fontWeight: FontWeight.w700,
-      );
+    fontFamily: 'Cairo',
+    color: colors(context).headingColor,
+    fontSize: 18.sp,
+    fontWeight: FontWeight.w700,
+  );
 }
